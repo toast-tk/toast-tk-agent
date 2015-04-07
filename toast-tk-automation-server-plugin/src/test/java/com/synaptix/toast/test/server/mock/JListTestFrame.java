e48e24b4ab72042dbf94b130f761e4eac73c6d04
guillaume.person@synaptix-labs.com <guillaume.person@synaptix-labs.com>
2015-04-07 10:00:34 +0200

    debut JList

---
+++ toast-tk-automation-server-plugin/src/test/java/com/synaptix/toast/test/server/mock/JListTestFrame.java
@@ -0,0 +1,19 @@
+package com.synaptix.toast.test.server.mock;
+
+import javax.swing.JFrame;
+import javax.swing.JList;
+
+public class JListTestFrame extends JFrame {
+
+	private static final long serialVersionUID = 596546026271112299L;
+
+	private JList<String> jList;
+
+	public JListTestFrame() {
+		final String[] datas = new String[]{"one", "two", "three", "four"};
+		this.jList = new JList<String>(datas);
+		getContentPane().add(jList);
+		pack();
+		setVisible(true);
+	}
+}
