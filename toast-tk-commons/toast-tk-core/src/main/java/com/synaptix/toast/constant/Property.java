b49c883695bf937fc12cb99c6d669f7179d6e151
Sallah Kokaina <sallah.kokaina@gmail.com>
2016-08-10 00:52:06 +0100

    compilation issue - dependency to fest is not required

---
+++ toast-tk-commons/toast-tk-core/src/main/java/com/synaptix/toast/constant/Property.java
@@ -0,0 +1,62 @@
+package com.synaptix.toast.constant;
+
+
+public class Property {
+
+	public static final String DEFAULT_WEBAPP_ADDR_PORT = "http://localhost:9000";
+
+	public static final String WEBAPP_ADDR = "toast.webapp.addr";
+
+	public static final String WEBAPP_PORT = "toast.webapp.port";
+
+	public static final String TOAST_PLUGIN_DIR_PROP = "toast.plugin.dir";
+
+	public static final String MONGO_HOST = "toast.mongo.addr";
+
+	public static final String MONGO_PORT = "toast.mongo.port";
+
+	public static final String TOAST_RUNTIME_TYPE = "toast.runtime.type";
+
+	public static final String TOAST_RUNTIME_CMD = "toast.runtime.command";
+
+	public static final String TOAST_RUNTIME_AGENT = "toast.runtime.agent";
+
+	public static final String AGENT_JAR_NAME = "toast-tk-agent-standalone.jar";
+
+	public static final String TOAST_SUT_RUNNER_SH = "run_sut.sh";
+	
+	public static final String TOAST_SUT_RUNNER_BAT = "run_sut.bat";
+
+	public static final String REDPEPPER_AUTOMATION_SETTINGS_DEFAULT_DIR = "settings/toast_descriptor.json";
+
+	public static final String JNLP_RUNTIME_HOST = "toast.jnlp.runtime.host";
+
+	public static final String JNLP_RUNTIME_FILE = "toast.jnlp.runtime.file";
+
+	public static final String AGENT_DEBUG_AGRS = "toast.sut.debug.args";
+
+	public static final String TABLE_CRITERIA_SEPARATOR = ";";
+
+	public static final String TABLE_KEY_VALUE_SEPARATOR = "=";
+
+	public static final String JLIST_CRITERIA_SEPARATOR = ";";
+
+	public static final String DEFAULT_PARAM_SEPARATOR = ";";
+
+	public static final String DEFAULT_PARAM_INPUT_SEPARATOR = "<-";
+
+	public static final int TOAST_AGENT_PORT = 7676;
+	
+	public static final String ACTION_ITEM_VAR_REGEX = "(\\$\\w+)";
+	
+	public static final String ACTION_ITEM_REGEX = "\\{\\{([\\w:]+)\\}\\}";
+	
+	public static final String RESULT_FOLDER = "TestResult";
+	
+	public static final String TOAST_TEST_WEB_INIT_RECORDING_URL = "toast.web.recording.url";
+
+	public static final String TOAST_CHROMEDRIVER_PATH = "toast.chromedriver.path";
+	
+	public static final String TOAST_TEST_WEB_APP_URL = "toast.webapp.url";
+
+}
