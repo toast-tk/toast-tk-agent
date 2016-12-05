dccc0632b4c51250fc83d8fd62a219f8e088db41
Sallah Kokaina <sallah.kokaina@gmail.com>
2016-12-05 16:17:20 +0000

    feature(run): test scripts execution

--- toast-tk-plugin-loader/src/main/java/io/toast/tk/plugin/PluginLoader.java
+++ toast-tk-plugin-loader/src/main/java/io/toast/tk/plugin/PluginLoader.java
@@ -2,19 +2,16 @@ package io.toast.tk.plugin;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.lang.reflect.Method;
 import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.ServiceLoader;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.github.kristofa.servicepluginloader.ServicePlugin;
-import com.github.kristofa.servicepluginloader.ServicePluginClassPath;
-import com.github.kristofa.servicepluginloader.ServicePluginLoader;
-import com.github.kristofa.servicepluginloader.ServicePluginsClassPathProvider;
 import com.google.inject.Module;
 
 import io.toast.tk.agent.config.AgentConfigProvider;
@@ -23,17 +20,26 @@ public class PluginLoader {
 
 	static final Logger LOG = LoggerFactory.getLogger(PluginLoader.class);
 	private AgentConfigProvider configProvider;
-	
-	public PluginLoader(AgentConfigProvider configProvider){
-		this.configProvider = configProvider;
+
+	public PluginLoader(AgentConfigProvider provider) {
+		this.configProvider = provider;
 	}
 
-	public List<IAgentPlugin> loadPlugins() {
-		final String pluginsPath = this.configProvider.get().getPluginDir();
-		final ServicePluginsClassPathProvider pluginsClassPathProvider = buildServicePluginsClassPathProvider(pluginsPath);
-		final ServicePluginLoader<IAgentPlugin> bootPluginsLoader = new ServicePluginLoader<>(IAgentPlugin.class, pluginsClassPathProvider);
-		List<IAgentPlugin> agentPlugins = collectPlugins(bootPluginsLoader); //EXTRA ACTIONS HERE
-		return agentPlugins;
+	private static void addSoftwareLibrary(File file,
+			ClassLoader classLoader) throws Exception {
+	    Method method = URLClassLoader.class.getDeclaredMethod("addURL", new Class[]{URL.class});
+	    method.setAccessible(true);
+	    method.invoke(classLoader, new Object[]{file.toURI().toURL()});
+	}
+	
+	public  List<IAgentPlugin> loadPlugins(
+			ClassLoader classLoader) {
+		extendClassLoaderPath(configProvider.get().getPluginDir(), classLoader);
+		ServiceLoader<IAgentPlugin> loader = ServiceLoader.load(IAgentPlugin.class, classLoader);
+		List<IAgentPlugin> list = new ArrayList<>();
+		loader.forEach(list::add);
+		LOG.info("Found {} plugins !", list.size());
+		return list;
 	}
 	
 	public Module[] collectGuiceModules(List<IAgentPlugin> agentPlugins){
@@ -44,36 +50,22 @@ public class PluginLoader {
 		return pluginModules.toArray(new Module[]{});
 	}
 
-	private static List<IAgentPlugin> collectPlugins(
-			final ServicePluginLoader<IAgentPlugin> bootPluginsLoader) {
-		Collection<ServicePlugin<IAgentPlugin>> load = bootPluginsLoader.load();
-		for (ServicePlugin<IAgentPlugin> servicePlugin : load) {
-			IAgentPlugin plugin = servicePlugin.getPlugin();
-			plugin.boot();
-		}
-		return load.stream().map(ServicePlugin::getPlugin).collect(Collectors.toList());
-	}
-
-	private static ServicePluginsClassPathProvider buildServicePluginsClassPathProvider(
-			final String pluginDir) {
-		return new ServicePluginsClassPathProvider() {
-			@Override
-			public Collection<ServicePluginClassPath> getPlugins() {
-				URL[] collectJarsInDirector = collectJarsInDirector(new File(pluginDir), Thread
-						.currentThread().getContextClassLoader());
-				List<ServicePluginClassPath> plugins = new ArrayList<>();
-				for (URL jar : collectJarsInDirector) {
-					LOG.info("Found plugin jar {}", jar);
-					plugins.add(new ServicePluginClassPath(jar));
-				}
-				return plugins;
+	private static void  extendClassLoaderPath(
+			final String pluginDir,
+			ClassLoader classLoader) {
+		URL[] collectJarsInDirector = collectJarsInDirector(new File(pluginDir));
+		for (URL jar : collectJarsInDirector) {
+			LOG.info("Found plugin jar {}", jar);
+			try {
+				addSoftwareLibrary(new File(jar.toURI()), classLoader);
+			} catch (Exception e) {
+				LOG.error(e.getMessage(), e);
 			}
-		};
+		}
 	}
 
 	public static URL[] collectJarsInDirector(
-			File directory,
-			ClassLoader parent) {
+			File directory) {
 		List<URL> allJars = new ArrayList<>();
 		fillJarsList(allJars, directory);
 		return allJars.toArray(new URL[allJars.size()]);
