5c54e00d92f1298822ced5e16672f3cf5099ff84
NathCoco <nathan66330@gmail.com>
2017-06-01 17:06:58 +0200

    Evol(AsynHttpClient) Add SSL connexion

--- toast-tk-web-agent/src/main/java/io/toast/tk/agent/web/rest/AsyncHttpClientProvider.java
+++ toast-tk-web-agent/src/main/java/io/toast/tk/agent/web/rest/AsyncHttpClientProvider.java
@@ -3,11 +3,20 @@ package io.toast.tk.agent.web.rest;
 import com.google.common.base.Strings;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
+
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
+import io.netty.handler.ssl.util.SelfSignedCertificate;
 import io.toast.tk.agent.config.AgentConfig;
+
+import java.security.cert.CertificateException;
+
+import javax.net.ssl.SSLException;
 
 import org.asynchttpclient.AsyncHttpClientConfig;
 import org.asynchttpclient.DefaultAsyncHttpClient;
 import org.asynchttpclient.DefaultAsyncHttpClientConfig;
+import org.asynchttpclient.DefaultAsyncHttpClientConfig.Builder;
 import org.asynchttpclient.Realm;
 import org.asynchttpclient.Realm.AuthScheme;
 import org.asynchttpclient.proxy.ProxyServer;
@@ -29,22 +38,28 @@ public class AsyncHttpClientProvider implements Provider<DefaultAsyncHttpClient>
             String proxyPort = this.config.getProxyPort();
             int port = Strings.isNullOrEmpty(proxyPort ) ? -1 : Integer.parseInt(proxyPort);
             ProxyServer proxyServer = new ProxyServer.Builder(config.getProxyAdress(), port).build();
+
+            Builder builder = new DefaultAsyncHttpClientConfig.Builder()
+                    .setProxyServer(proxyServer);
             
             if(!Strings.isNullOrEmpty(config.getProxyUserName()) && !Strings.isNullOrEmpty(config.getProxyUserPswd()) ){
                 Realm realm = new Realm.Builder(config.getProxyUserName(), config.getProxyUserPswd())
                 		.setScheme(AuthScheme.BASIC)
                 		.build();
                 
-                AsyncHttpClientConfig cf = new DefaultAsyncHttpClientConfig.Builder()
-                        .setProxyServer(proxyServer)
-                        .setRealm(realm)
-                        .build();
-                return new DefaultAsyncHttpClient(cf);
+                builder.setRealm(realm);
             }
             
-            AsyncHttpClientConfig cf = new DefaultAsyncHttpClientConfig.Builder()
-                    .setProxyServer(proxyServer).build();
-            return new DefaultAsyncHttpClient(cf);
+			try {
+				SelfSignedCertificate ssc = new SelfSignedCertificate();
+				 final SslContext sslCtx = SslContext.newServerContext(ssc.certificate(), ssc.privateKey());
+				builder.setSslContext(sslCtx);
+			} catch (SSLException | CertificateException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+            return new DefaultAsyncHttpClient(builder.build());
         }
         return new DefaultAsyncHttpClient();
     }
